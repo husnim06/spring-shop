84a98730ee1e91e3341f461d4a3c7b4c707664c8
husnim <kazancevvlad572@gmail.com>
2025-08-12 20:21:09 +0500

    при создании пользователя ему присваивается корзина, исправлены ошибки при получении пользователя по имени или id

--- src/main/java/ru/husnim/spring_shop/service/UserService.java
+++ src/main/java/ru/husnim/spring_shop/service/UserService.java
@@ -1,11 +1,13 @@
 package ru.husnim.spring_shop.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 import ru.husnim.spring_shop.dto.UserDTO;
 import ru.husnim.spring_shop.dto.UserResponseDTO;
+import ru.husnim.spring_shop.model.Cart;
 import ru.husnim.spring_shop.model.User;
 import ru.husnim.spring_shop.repository.UserRepository;
 
@@ -14,6 +16,9 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private CartService cartService;
 
     public List<UserResponseDTO> getAllUsers() {
         return userRepository.findAll().stream()
@@ -27,31 +32,37 @@ public class UserService {
     }
 
     public UserResponseDTO getUserById(Long id) {
-        return (UserResponseDTO) userRepository.findById(id).stream()
-                .map(user -> {
-                    UserResponseDTO dto = new UserResponseDTO();
-                    dto.setId(user.getId());
-                    dto.setName(user.getName());
-                    return dto;
-                })
-                .collect(Collectors.toList());
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Пользователь не найден"));
+
+        UserResponseDTO dto = new UserResponseDTO();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+
+        return dto;
     }
 
     public UserResponseDTO getUserByName(String name) {
-        return (UserResponseDTO) userRepository.findByName(name).stream()
-                .map(user -> {
-                    UserResponseDTO dto = new UserResponseDTO();
-                    dto.setId(user.getId());
-                    dto.setName(user.getName());
-                    return dto;
-                })
-                .collect(Collectors.toList());
+        User user = userRepository.findByName(name)
+                .orElseThrow(() -> new RuntimeException("Пользователь не найден"));
+
+        UserResponseDTO dto = new UserResponseDTO();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+
+        return dto;
     }
 
     public User createUser(UserDTO userDTO) {
         User user = new User();
         user.setName(userDTO.getName());
         user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+
+        // Создаем элемент корзины и присваиваем пользователю
+        Cart cart = cartService.createCart();
+        user.setCart(cart);
+
         return userRepository.save(user);
     }
 
