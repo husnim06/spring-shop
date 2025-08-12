94ca2e02095fd413283adbaf19e0b002ecf1902a
husnim <kazancevvlad572@gmail.com>
2025-08-12 20:22:37 +0500

    добавлены поля с корзиной и паролем, изменена валидация

--- src/main/java/ru/husnim/spring_shop/model/User.java
+++ src/main/java/ru/husnim/spring_shop/model/User.java
@@ -1,9 +1,13 @@
 package ru.husnim.spring_shop.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -20,12 +24,22 @@ public class User {
     private Long id;
 
     @Size(min = 1)
+    @Column(unique = true)
     @NotBlank(message = "Имя не может быть пустым")
     private String name;
 
     @NotBlank
     @Size(min = 1)
+    @Column(unique = true)
     @Email(message = "Некорректный email")
     private String email;
+    
+    @NotBlank
+    @Size(min = 1)
+    private String password;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id", referencedColumnName = "id")
+    private Cart cart;
     
 }
