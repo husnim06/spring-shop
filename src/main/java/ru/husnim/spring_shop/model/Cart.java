9811796f65f1c74f8b99691467eaa155da21e40c
husnim <kazancevvlad572@gmail.com>
2025-08-12 19:01:45 +0500

    добавлены модели для корзины

---
+++ src/main/java/ru/husnim/spring_shop/model/Cart.java
@@ -0,0 +1,26 @@
+package ru.husnim.spring_shop.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private Set<CartItem> items = new HashSet<>();
+
+}
---
+++ src/main/java/ru/husnim/spring_shop/model/CartItem.java
@@ -0,0 +1,25 @@
+package ru.husnim.spring_shop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Entity
+@Data
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    @Positive
+    private int quantity; // Количество товара в корзине
+    
+}
