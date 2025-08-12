58c09f86f054e2f1da5b0e1aa16cbb51851303ac
husnim <kazancevvlad572@gmail.com>
2025-08-12 19:02:47 +0500

    Создан сервис для управления корзиной

---
+++ src/main/java/ru/husnim/spring_shop/service/CartService.java
@@ -0,0 +1,59 @@
+package ru.husnim.spring_shop.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.husnim.spring_shop.model.Cart;
+import ru.husnim.spring_shop.model.CartItem;
+import ru.husnim.spring_shop.model.Product;
+import ru.husnim.spring_shop.repository.CartRepository;
+import ru.husnim.spring_shop.repository.ProductRepository;
+
+@Service
+public class CartService {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+    
+    public Cart createCart() {
+        Cart cart = new Cart();
+        return cartRepository.save(cart);
+    }
+
+    public Cart getCart(Long cartId) {
+        return cartRepository.findById(cartId)
+                .orElseThrow(() -> new RuntimeException("Корзина не найдена"));
+    }
+
+    public void addItemToCart(Long cartId, Long productId, int quantity) {
+        Cart cart = getCart(cartId);
+        
+        // Загружаем продукт по ID
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException("Продукт не найден"));
+
+        // Создаем новый элемент корзины
+        CartItem cartItem = new CartItem();
+        cartItem.setProduct(product); // Устанавливаем загруженный продукт
+        cartItem.setQuantity(quantity);
+        
+        // Добавляем элемент в корзину
+        cart.getItems().add(cartItem);
+        cartRepository.save(cart);
+    }
+
+    public void removeItemFromCart(Long cartId, Long cartItemId) {
+        Cart cart = getCart(cartId);
+        cart.getItems().removeIf(item -> item.getId().equals(cartItemId));
+        cartRepository.save(cart);
+    }
+
+    public void clearCart(Long cartId) {
+        Cart cart = getCart(cartId);
+        cart.getItems().clear();
+        cartRepository.save(cart);
+    }
+    
+}
