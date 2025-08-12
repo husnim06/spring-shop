785a18ffe11bcf83ec672fdb402fcd1e88845029
husnim <kazancevvlad572@gmail.com>
2025-08-12 18:22:08 +0500

    Добавлены категории товаров

---
+++ src/main/java/ru/husnim/spring_shop/model/Category.java
@@ -0,0 +1,27 @@
+package ru.husnim.spring_shop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min = 1)
+    @Column(unique = true)
+    @NotBlank(message = "Название не может быть пустым")
+    private String name;
+
+}
--- src/main/java/ru/husnim/spring_shop/model/Product.java
+++ src/main/java/ru/husnim/spring_shop/model/Product.java
@@ -4,10 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.Data;
 
 @Data
@@ -21,13 +26,21 @@ public class Product {
 
     @Size(min = 1)
     @NotBlank(message = "Название не может быть пустым")
-    public String name;
+    private String name;
 
     @Positive(message = "Цена должна быть положительной")
-    public float price;
+    private float price;
 
     @NotBlank
     @Size(min = 1)
     private String description;
+
+    @ManyToMany
+    @JoinTable(
+            name = "product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
 
 }
