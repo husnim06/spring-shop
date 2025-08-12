fffbfde162351c3944a9c29722d5322d1be3767a
husnim <kazancevvlad572@gmail.com>
2025-08-12 20:23:03 +0500

    Добавлено поле с паролем, изменена валидация

--- src/main/java/ru/husnim/spring_shop/dto/UserDTO.java
+++ src/main/java/ru/husnim/spring_shop/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package ru.husnim.spring_shop.dto;
 
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -9,12 +10,18 @@ import lombok.Data;
 public class UserDTO {
 
     @Size(min = 1)
+    @Column(unique = true)
     @NotBlank(message = "Имя не может быть пустым")
     private String name;
 
     @NotBlank
     @Size(min = 1)
+    @Column(unique = true)
     @Email(message = "Некорректный email")
     private String email;
+    
+    @NotBlank
+    @Size(min = 1)
+    private String password;
 
 }
